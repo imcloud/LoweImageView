7aa3aa17110bec877c573e5c29c9669efc441861
Leonhardt <jinwenxiu@pinwine.cn>
2015-06-10 16:21:16 +0800

    首次创建

---
+++ app/src/main/java/cn/sz/leonhardt/loweimageview/app/MainActivity.java
@@ -0,0 +1,28 @@
+package cn.sz.leonhardt.loweimageview.app;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        restoreActionBar();
+    }
+
+    /**
+     * 载入actionbar
+     */
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+    }
+
+}
